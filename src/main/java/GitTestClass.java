75cbb1092d4a1e2cb6c602bd2eed8e4b56f2b6cf
nsiva02 <welcome@DESKTOP-L2OG0MP>
2020-10-07 18:05:05 +0530

    Commit Sdetgit new Project

---
+++ src/main/java/GitTestClass.java
@@ -0,0 +1,18 @@
+
+public class GitTestClass {
+
+	public static void main(String[] args) {
+		
+		String s1="hungry";
+		
+		if(s1.equals("hungry")) {
+			System.out.println("I have something to eat");
+		}
+		else {
+			System.out.println("I am not hungry");
+		}
+		
+
+	}
+
+}
